057b46ff86567ce90837a08031eef45c1b0c28ab
Sytronik <sytronik210@gmail.com>
2026-01-29 22:11:45 +0900

    Refactor build process to copy Info.plist from source instead of generating it dynamically

--- build.sh
+++ build.sh
@@ -24,49 +24,8 @@ mkdir -p "${RESOURCES_DIR}"
 # Copy executable
 cp "${BUILD_DIR}/Teumnirm" "${MACOS_DIR}/"
 
-# Create Info.plist
-cat > "${CONTENTS_DIR}/Info.plist" << 'EOF'
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>CFBundleDevelopmentRegion</key>
-    <string>ko</string>
-    <key>CFBundleExecutable</key>
-    <string>Teumnirm</string>
-    <key>CFBundleIconFile</key>
-    <string>AppIcon</string>
-    <key>CFBundleIdentifier</key>
-    <string>com.teumnirm.app</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>CFBundleName</key>
-    <string>Teumnirm</string>
-    <key>CFBundleDisplayName</key>
-    <string>틈니름</string>
-    <key>CFBundlePackageType</key>
-    <string>APPL</string>
-    <key>CFBundleShortVersionString</key>
-    <string>1.0.0</string>
-    <key>CFBundleVersion</key>
-    <string>1</string>
-    <key>LSMinimumSystemVersion</key>
-    <string>13.0</string>
-    <key>LSUIElement</key>
-    <true/>
-    <key>NSHighResolutionCapable</key>
-    <true/>
-    <key>NSHumanReadableCopyright</key>
-    <string>Copyright © 2026. All rights reserved.</string>
-    <key>NSLocalNetworkUsageDescription</key>
-    <string>Teumnirm needs local network access to communicate with your Philips Hue bridge.</string>
-    <key>NSBonjourServices</key>
-    <array>
-        <string>_hue._tcp</string>
-    </array>
-</dict>
-</plist>
-EOF
+# Copy Info.plist
+cp "Info.plist" "${CONTENTS_DIR}/"
 
 echo "Build complete!"
 echo "App bundle created at: ${APP_DIR}"
